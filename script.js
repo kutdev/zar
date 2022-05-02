898444141768a2da26ddd85640674d98ca9c329b
kutdev <78854154+kutdev@users.noreply.github.com>
2022-05-02 12:20:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+function zar(){
+    
+    var sayi = Math.floor(Math.random() * 6);
+    var sayi2 = Math.floor(Math.random() * 6);
+     if (sayi != 0 && sayi2 != 0) {
+        document.getElementById("zar").innerHTML = sayi;
+        document.getElementById("zar2").innerHTML = sayi2;
+     }
+        else{
+             sayi = Math.floor(Math.random() * 6);
+             sayi2 = Math.floor(Math.random() * 6);
+        }
+        
+    
+    
+}
+
+function refresh(){
+    location.reload();
+}
